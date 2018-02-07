40705acb420b3550af84a67796ee17834cab1456
Carmen Ma. Soto <carsoto8691@gmail.com>
2018-02-07 11:17:31 -0430

    Versión HTML

--- index.php
+++
@@ -1,9 +0,0 @@
-<ul>
-<li><a href="http://localhost/texto-pago/2.php" target="_blank">Formulario 2</a></li>
-<li><a href="http://localhost/texto-pago/3.php" target="_blank">Formulario 3</a></li>
-<li><a href="http://localhost/texto-pago/7.php" target="_blank">Formulario 7</a></li>
-<li><a href="http://localhost/texto-pago/11.php" target="_blank">Formulario 11</a></li>
-</ul>
-
-
-
